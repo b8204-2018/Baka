ae0ec6471bf05e48f172b8670bed023408a4a0e3
BakaNia <44308272+BakaNia@users.noreply.github.com>
2019-06-28 13:52:36 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,72 +1,103 @@
 #include <iostream>
-#include "string.h"
 
-using  namespace std;
+using namespace std;
 
 class Sort {
 public:
+    virtual void setArray(int n) = 0;
     virtual string sortName() = 0;
-    virtual void sort(int *arr, int n) = 0;
 };
 
 class Cocktail : public Sort {
+    int n;
+    int* arr;
 public:
     string sortName() override {
-        return "Cocktail Sort";
+        return "CocktailSort";
     }
-    void sort (int *arr, int n) {
-        bool swapped = true;
-        int start = 0;
-        int end = n - 1;
 
-        while (swapped) {
-            swapped = false;
-            for (int i = start; i < end; ++i) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr[i], arr[i + 1]);
-                    swapped = true;
-                }
-            }
-            if (!swapped)
-                break;
-            swapped = false;
-            --end;
-            for (int i = end - 1; i >= start; --i) {
-                if (arr[i] > arr[i + 1]) {
-                    swap(arr[i], arr[i + 1]);
-                    swapped = true;
-                }
-            }
-            ++start;
-        }
-    } ;
+    void setArray(int n) override;
+    void Sort();
+    void PrintArray(class Sort &c1);
 };
 
-void printSortedArray(Sort &sort, int *arr, int n){
-    int *res = new int[n];
-    memcpy(res, arr, n * sizeof(int));
-    sort.sortName();
-    sort.sort(res, n);
+
+void Cocktail::setArray(int n) {
+    this->n = n;
+    arr = new int[n];
+    for (int i = 0; i < this->n; i++) {
+        cout << "Array[" << i << "] - ";
+        cin >> arr[i];
+    }
+};
+
+
+void Cocktail::Sort() {
+
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+
+    while (swapped) {
+        // reset the swapped flag on entering
+        // the loop, because it might be true from
+        // a previous iteration.
+        swapped = false;
+
+        // loop from left to right same as
+        // the bubble sort
+        for (int i = start; i < end; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        // if nothing moved, then array is sorted.
+        if (!swapped)
+            break;
+
+        // otherwise, reset the swapped flag so that it
+        // can be used in the next stage
+        swapped = false;
+
+        // move the end point back by one, because
+        // item at the end is in its rightful spot
+        --end;
+
+        // from right to left, doing the
+        // same comparison as in the previous stage
+        for (int i = end - 1; i >= start; --i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+
+        // increase the starting point, because
+        // the last stage would have moved the next
+        // smallest number to its rightful spot.
+        ++start;
+    }
+
+};
+
+void Cocktail::PrintArray(class Sort &c1) {
+    cout << c1.sortName()<< endl;
     for (int i = 0; i < n; i++)
-        cout << " " << res[i];
+        cout << arr[i] << " ";
     cout << endl;
 }
 
 int main() {
 
-    Cocktail cocktail;
+    Cocktail c1;
     int n;
-    cout << "Vvedite razmernost' massiva : " << endl;
+    cout << "Number of array elements - ";
     cin >> n;
-    cout << "Nachalnii massiv : " << endl;
-    int *userArray = new int[n];
-    for(size_t i = 0; i < n; i++){
-        userArray[i] = rand() % 100;
-        cout << userArray[i] << " ";
-    }
+    c1.setArray(n);
+    c1.PrintArray(c1);
+    c1.Sort();
+    c1.PrintArray(c1);
 
-    cout << endl;
-    printSortedArray(cocktail, userArray, n);
-
-    return 0;
 }
