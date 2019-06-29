94bfd039f879af19ad5d5d6a09cdb1206c2c18aa
BakaNia <44308272+BakaNia@users.noreply.github.com>
2019-06-29 16:54:12 +1000

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -6,23 +6,24 @@
 #include <list>
 #include <deque>
 #include "opz.h"
-using namespace std;
-int main() {
 
-    string s = "( 22 + 55 * 22 ) /  12";
-    ExpressionParser n ;
+using namespace std;
+
+int main() {
+    string s = " 5 + 2 ";
+    ExpressionParser n;
     Calculator calculator;
     list<string> expression;
     try {
-         expression= n.parse(s);
-    }catch(logic_error e) {
-        cout << e.what()<<endl; //некорректное выражение или скобки несогласованы
+        expression = n.parse(s);
+    } catch (logic_error e) {
+        cout << e.what() << endl; //некорректное выражение или скобки несогласованы
     }
     bool flag = n.flag;
     if (flag) {
-        for (string x : expression) {cout<<x;} //for each печатает
-        cout<<endl;
-        cout<<calculator.calc(expression);
+        for (string x : expression) { cout << x; } //for each печатает
+        cout << endl;
+        cout << calculator.calc(expression);
     }
 
     return 0;
